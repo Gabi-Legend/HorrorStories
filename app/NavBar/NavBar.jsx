85f2cc43ad79184bf455f27bea39adfcc4be0d41
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-08-03 13:49:23 +0300

    Css for buttons

--- app/NavBar/NavBar.jsx
+++ app/NavBar/NavBar.jsx
@@ -3,7 +3,13 @@ import styles from "@/app/NavBar/NavBar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.navigation}>
-      <h1 className={styles.logo}>Horror Stories</h1>
+      <h1 className={styles.logo}>
+        <a href="/">Horror Stories</a>
+      </h1>
+      <div className={styles.signSection}>
+        <button className={styles.signBtn}>Sign up</button>
+        <button className={styles.signBtn}>Log in</button>
+      </div>
     </nav>
   );
 }
