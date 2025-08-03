7edf088cd698166542186c634b7e7444503a8d8d
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-08-03 17:53:02 +0300

    Adding better responsive elements

--- app/components/NavBar/NavBar.jsx
+++ app/components/NavBar/NavBar.jsx
@@ -1,17 +1,43 @@
-import styles from "@/app/components/NavBar/NavBar.module.css";
+"use client";
+
+import { useState } from "react";
+import styles from "./NavBar.module.css";
 import Link from "next/link";
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navigation}>
       <h1 className={styles.logo}>
-        <a href="/">Horror Stories</a>
+        <Link href="/">Horror Stories</Link>
       </h1>
-      <div className={styles.signSection}>
-        <button className={styles.signBtn}>
-          <Link href="/signup">Sign Up</Link>
-        </button>
-        <button className={styles.signBtn}>Log in</button>
+
+      <button
+        className={styles.burger}
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle menu"
+      >
+        <span
+          className={menuOpen ? styles.burgerLineActive : styles.burgerLine}
+        ></span>
+        <span
+          className={menuOpen ? styles.burgerLineActive : styles.burgerLine}
+        ></span>
+        <span
+          className={menuOpen ? styles.burgerLineActive : styles.burgerLine}
+        ></span>
+      </button>
+
+      <div
+        className={`${styles.signSection} ${menuOpen ? styles.showMenu : ""}`}
+      >
+        <Link href="/signup" className={styles.signBtn}>
+          Sign Up
+        </Link>
+        <Link href="/login" className={styles.signBtn}>
+          Log in
+        </Link>
       </div>
     </nav>
   );
