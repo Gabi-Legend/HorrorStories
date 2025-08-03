ee6774287c1edb06536c02d3552b9a6a5994190e
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-08-03 14:10:34 +0300

    The footer is done

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,5 +1,16 @@
 import styles from "@/app/components/Footer/Footer.module.css";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div>© 2025 Horror Stories. All rights reserved.</div>
+      <div className={styles.links}>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact</a>
+        <a href="/privacy">Privacy Policy</a>
+        <a href="/terms">Terms of Service</a>
+      </div>
+      <div className={styles.socials}>{/* Icone sociale, dacă vrei */}</div>
+    </footer>
+  );
 }
