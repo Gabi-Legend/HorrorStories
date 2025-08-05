f9f9da7f65e49573505265425e5abdb1b331e7db
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-08-05 14:27:55 +0300

    Navbar for story page

---
+++ app/components/storyNav/StoryNav.jsx
@@ -0,0 +1,10 @@
+import styles from "./StoryNav.module.css";
+
+export default function StoryNav() {
+  return (
+    <nav>
+      <h1>Horror Stories</h1>
+      <button>Create story</button>
+    </nav>
+  );
+}
